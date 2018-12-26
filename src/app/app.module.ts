1c2e6e34928eabb02c795b892c5a5f256c95ecab
gustavosinbandera1 <gustavosinbandera1@hotmail.com>
2018-12-26 16:58:12 -0500

    se agrego el router y el componente detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+/*define the routes*/
+const ROUTES = [
+  {
+    path: '',
+    redirectTo: 'heroes',
+    pathMatch: 'full'
+  },
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(ROUTES) /*add routes to the app*/
   ],
   providers: [],
   bootstrap: [AppComponent]
